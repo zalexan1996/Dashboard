3cc064096d3d85e1cf5470e8c61d40fff79b153b
Zach Alexander <zalexan1996@gmail.com>
2022-05-12 16:30:41 -0500

    Added Modal

---
+++ Dashboard/wwwroot/js/modal.js
@@ -0,0 +1,18 @@
+﻿
+function initializeModal(dialog, reference) {
+    dialog.addEventListener("close", async e => {
+        await reference.invokeMethodAsync("Close");
+    });
+}
+
+function openModal(dialog) {
+    if (!dialog.open) {
+        dialog.showModal();
+    }
+}
+
+function closeModal(dialog) {
+    if (dialog.open) {
+        dialog.close();
+    }
+}
